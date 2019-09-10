29a452c8be98fc5f5a721dfd86a6b2e4d77204ca
Tapaswini kar <kar.tapaswini1111@gmail.com>
2019-09-10 23:32:56 +0530

    Add detail view for each blog post.

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,8 @@ urlpatterns= [
     #127.0.0.1:8000
     path('',views.post_list,name='post_list'),
 
+    #127.0.0.1.80000/post/2
+    path('post/<int:pk>/',views.post_detail , name='post_detail')
+
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.utils import  timezone
 from  .models import Post
+from  django.shortcuts import get_object_or_404
 
 
 
@@ -9,3 +10,10 @@ def post_list(request):
     posts= Post.objects.all().order_by('-published_date')
     stuff_for_frontend={'posts':posts}
     return render(request,'blog/post_list.html',stuff_for_frontend)
+
+def post_detail(request,pk):
+    post=get_object_or_404(Post,pk=pk)
+    stuff_for_frontend= {'post':post}
+
+    return render(request,'blog/post_detail.html',stuff_for_frontend)
+
